4031ff9838cbfff20e509091f0a623a06f4f4856
seondal <sseondal@gmail.com>
2024-04-17 01:03:22 +0900

    ⚙️ set :  modify eas for apk

--- App.js
+++ App.js
@@ -6,8 +6,8 @@ export default function App() {
   return (
     <WebView
       // source={{ uri: "https://www.posepicker.site" }}
-      source={{ uri: "https://develop.posepicker.site" }}
       // source={{ uri: "http://192.168.0.4:3000" }}
+      source={{ uri: "https://develop.posepicker.site" }}
       style={{
         flex: 1,
         marginTop: Constants.statusBarHeight,
