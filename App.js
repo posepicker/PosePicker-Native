c25f73f3d12cb31f38bdf0789172e980ed42dbf1
seondal <sseondal@gmail.com>
2024-04-17 21:56:49 +0900

    ✨ feat :  배포용으로 변경

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 import * as React from "react";
 import { WebView } from "react-native-webview";
-import Constants from "expo-constants";
 
 export default function App() {
   return (
     <WebView
-      // source={{ uri: "https://www.posepicker.site" }}
+      source={{ uri: "https://www.posepicker.site" }}
       // source={{ uri: "http://192.168.0.4:3000" }}
-      source={{ uri: "https://develop.posepicker.site" }}
+      // source={{ uri: "https://develop.posepicker.site" }}
       style={{
         flex: 1,
       }}
